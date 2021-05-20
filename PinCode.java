beef70f4c325c5bc33ecd22932aa3f6fec5bcd8f
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-20 07:51:03 +0530

    White Space validated in pin

--- PinCode.java
+++ PinCode.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class PinCode {
 	
 	public static boolean validatePin(String pinCode) {
-		String regex = "^[1-9]{1}[0-9]{2}[0-9]{3}$";
+		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
 		
 		Pattern p = Pattern.compile(regex);
 		
@@ -24,5 +24,7 @@ public class PinCode {
 		 System.out.println(pin2+" "+validatePin(pin2));
 		 String pin3 = "400088B";
 		 System.out.println(pin3+" "+validatePin(pin3));
+		 String pin4 = "400 088";
+		 System.out.println(pin4+" "+validatePin(pin4));
 	}
 }
